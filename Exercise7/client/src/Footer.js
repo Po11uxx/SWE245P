d75cd5745abcae923d935b1591efc1e54f882808
mac <junepollux@163.com>
2025-11-06 18:49:43 -0800

    exercise7

---
+++ Exercise7/client/src/Footer.js
@@ -0,0 +1,16 @@
+import './Footer.css'
+
+function Footer() {
+    return (
+        <>
+            <h2>Fixed/Sticky Footer Example</h2>
+            <p>The footer is placed at the bottom of the page.</p>
+
+            <div className="footer">
+                <p>Footer</p>
+            </div>
+        </>
+    );
+}
+
+export default Footer;
